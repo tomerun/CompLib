38c34ec352e69a8ca9f800b634fabd3ba4d2424b
tomerun <tomerunmail@gmail.com>
2017-12-24 23:31:16 +0900

    refine chineseRemainder

--- java/Integer.java
+++ java/Integer.java
@@ -1,19 +1,36 @@
 
-	static int chineseRemainder(int[] mod, int[] rem) {
-		int m1 = mod[0];
-		int r1 = rem[0];
+	static int chineseRemainder(long[] mod, long[] rem) {
+		long m1 = mod[0];
+		long r1 = rem[0];
 		for (int i = 1; i < mod.length; ++i) {
-			int m2 = mod[i];
-			int r2 = rem[i];
+			long m2 = mod[i];
+			long r2 = rem[i];
 			r1 = chineseRemainder(m1, r1, m2, r2);
 			m1 *= m2;
 		}
 		return r1;
 	}
 
-	static int chineseRemainder(int m1, int r1, int m2, int r2) {
-		int A = ((r2 - r1) % m2 + m2) * inv(m1, m2) % m2;
+	static long chineseRemainder(long m1, long r1, long m2, long r2) {
+		long A = ((r2 - r1) % m2 + m2) * inv(m1, m2) % m2;
 		return (A * m1 + r1) % (m1 * m2);
+	}
+
+	static long inv(long v, int mod) {
+		return pow(v, mod - 2, mod);
+	}
+
+	static long pow(long v, int p, int mod) {
+		if (p == 0) return 1;
+		if (p == 1) return v;
+		long ret = pow(v, p / 2, mod);
+		ret *= ret;
+		ret %= mod;
+		if (p % 2 == 1) {
+			ret *= v;
+			ret %= mod;
+		}
+		return ret;
 	}
 
 	static int totient(int v) {
