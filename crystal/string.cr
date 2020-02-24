47331db601d9096c90a844ab6361ef36683ffd89
tomerun <tomerunmail@gmail.com>
2020-02-24 21:31:15 +0900

    z-algo

---
+++ crystal/string.cr
@@ -0,0 +1,34 @@
+def z_algo(p : Array(T), t : Array(T)) forall T
+  ret = [] of Int32
+  return ret if p.size > t.size
+  s = p + [T.zero] + t
+  z = Array.new(s.size, 0)
+  z[0] = 0
+  left = 0
+  right = 0
+  1.upto(s.size - 1) do |i|
+    if i >= right
+      left = i
+      right = i
+      while right < s.size && s[right] == s[right - left]
+        right += 1
+      end
+      z[i] = right - left
+    else
+      k = i - left
+      if z[k] < right - i
+        z[i] = z[k]
+      else
+        left = i
+        while right < s.size && s[right] == s[right - left]
+          right += 1
+        end
+        z[i] = right - left
+      end
+    end
+    if p.size < i && z[i] == p.size
+      ret << i - p.size - 1
+    end
+  end
+  return ret
+end
