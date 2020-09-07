34c31206d7afb1d1480848fdfc5c2cd2e1188584
tomerun <tomerunmail@gmail.com>
2020-09-07 22:28:53 +0900

    zero

--- crystal/segtree.cr
+++ crystal/segtree.cr
@@ -1,25 +1,25 @@
 class SegTree(T)
-  # alias Op = Proc(T, T, T)
   @ar : Array(T)
   @size : Int32
   @op : Proc(T, T, T)
+  @zero : T
 
-  def initialize(s : Int32, @op : Proc(T, T, T))
+  def initialize(s : Int32, @op : Proc(T, T, T), @zero : T)
     @size = 1
     while @size < s
       @size *= 2
     end
-    @ar = Array.new(@size * 2, T.zero)
+    @ar = Array.new(@size * 2, @zero)
   end
 
-  def initialize(init : Array(T), @op : Proc(T, T, T))
+  def initialize(init : Array(T), @op : Proc(T, T, T), @zero : T)
     @size = 1
     while @size < init.size
       @size *= 2
     end
-    @ar = Array.new(@size, T.zero)
+    @ar = Array.new(@size, @zero)
     @ar.concat(init)
-    @ar.concat([T.zero] * (@size - init.size))
+    @ar.concat([@zero] * (@size - init.size))
     (@size - 1).downto(1) do |i|
       @ar[i] = @op.call(@ar[i * 2], @ar[i * 2 + 1])
     end
@@ -34,7 +34,7 @@ class SegTree(T)
       return @ar[node]
     end
     mid = (left + right) // 2
-    ret = T.zero
+    ret = @zero
     if lo < mid
       ret = get(lo, hi, node * 2, left, mid)
     end
@@ -64,7 +64,7 @@ class SegTree(T)
 end
 
 data = [1, 2, 3, 4, 5, 6, 1, 2]
-segtree = SegTree(Int32).new(data, ->(x : Int32, y : Int32) { {x, y}.max })
+segtree = SegTree(Int32).new(data, ->(x : Int32, y : Int32) { {x, y}.max }, 0)
 2.upto(data.size) do |i|
   puts segtree.get(1, i)
 end
