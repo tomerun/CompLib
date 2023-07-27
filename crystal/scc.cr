389e939ecb341659778a80bbb0555a8583cc0ae4
tomerun <tomerunmail@gmail.com>
2023-07-28 02:22:13 +0900

    update scc

--- crystal/scc.cr
+++ crystal/scc.cr
@@ -1,31 +1,43 @@
-stack = [] of Int32
-visited = Array.new(n, false)
-n.times do |i|
-  scc_dfs(g, stack, visited, i)
-end
-visited.fill(false)
-groups = [] of Array(Int32)
-stack.reverse.each do |i|
-  next if visited[i]
-  group = [] of Int32
-  scc_dfs_rev(g_rev, group, visited, i)
-  groups << group
-end
+class SCC
+  getter :groups # Array(Array(Int32)) トポロジカルソートされた強連結成分のリスト
 
-def scc_dfs(g, stack, visited, cur)
-  return if visited[cur]
-  visited[cur] = true
-  g[cur].each do |c|
-    scc_dfs(g, stack, visited, c)
+  def initialize(g : Array(Array(Int32)))
+    stack = [] of Int32
+    visited = Array.new(g.size, false)
+    g.size.times do |i|
+      dfs(g, stack, visited, i)
+    end
+    visited.fill(false)
+    g_rev = Array.new(g.size) { [] of Int32 }
+    g.size.times do |v|
+      g[v].each do |u|
+        g_rev[u] << v
+      end
+    end
+    @groups = [] of Array(Int32)
+    stack.reverse.each do |i|
+      next if visited[i]
+      group = [] of Int32
+      dfs_rev(g_rev, group, visited, i)
+      groups << group
+    end
   end
-  stack << cur
-end
 
-def scc_dfs_rev(g_rev, list, visited, cur)
-  return if visited[cur]
-  visited[cur] = true
-  g_rev[cur].each do |c|
-    scc_dfs_rev(g_rev, list, visited, c)
+  def dfs(g, stack, visited, cur)
+    return if visited[cur]
+    visited[cur] = true
+    g[cur].each do |c|
+      dfs(g, stack, visited, c)
+    end
+    stack << cur
   end
-  list << cur
+
+  def dfs_rev(g_rev, group, visited, cur)
+    return if visited[cur]
+    visited[cur] = true
+    g_rev[cur].each do |c|
+      dfs_rev(g_rev, group, visited, c)
+    end
+    group << cur
+  end
 end
